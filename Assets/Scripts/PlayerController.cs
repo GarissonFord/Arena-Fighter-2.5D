99f776eb6a5471008ccf05c58f59eb9bd0126f5f
Gary DiCiano <garydiciano4@gmail.com>
2017-05-31 14:42:01 -0400

    Player Animation Complete
    Properly detects when touching the ground and animations have been set accordingly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour {
 
 	Animator animator;
 
+	//Variables to check for jump and double-jump
+	public bool grounded, jumped;
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
@@ -20,11 +23,14 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		moveHorizontal = Input.GetAxis("Horizontal");
+		//Gets input from arrow keys, A-D, or space bar
+		moveHorizontal = Input.GetAxis ("Horizontal");
 		moveVertical = Input.GetAxis ("Vertical");
 
-		rb.MovePosition (transform.position + new Vector3 (moveHorizontal, moveVertical, 0.0f));
+		//Allows fluid movement but fall speed is stilltoo slow 
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
 
+		//Updates animator
 		if (moveHorizontal != 0.0f) 
 		{
 			animator.SetBool ("IsMoving", true);
@@ -32,6 +38,35 @@ public class PlayerController : MonoBehaviour {
 		else 
 		{
 			animator.SetBool ("IsMoving", false);
+		}
+
+		//Checks for jump
+		if (moveVertical != 0.0f) 
+		{
+			grounded = false;
+			jumped = true;
+			animator.SetBool ("IsJumped", true);
+		}
+
+		if (jumped) 
+		{
+			rb.velocity = new Vector3 (movement.x, jumpHeight, 0.0f);;
+		}
+
+		//Updates movement
+		rb.velocity = movement * speed;
+	}
+
+	//Checks if the player is touching the ground
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag ("Ground")) 
+		{
+			//Booleans and animations are updated accordingly 
+			grounded = true;
+			jumped = false;
+			animator.SetBool ("IsGrounded", true);
+			animator.SetBool ("IsJumped", false);
 		}
 	}
 }
